a574a79d6a38fa637bbe3e3e10ead4a41fc51095
Andrew Meyer <ameyer@servercentral.com>
2018-12-03 21:18:46 -0600

    first working upload of watson.fish

--- watson.fish
+++ watson.fish
@@ -1,3 +1,6 @@
+# copy this into ~/.config/fish/completions/ to enable autocomplet for the watson time tracker
+# Author: Andrew Meyer
+#
 function __fish_watson_needs_sub -d "provides a list of sub commands"
   set cmd (commandline -opc)
   if [ (count $cmd) -eq 1 -a $cmd[1] = 'watson' ]
@@ -8,7 +11,7 @@ end
 
 function __fish_watson_using_command
   set cmd (commandline -opc)
-  if [ (count $cmd) -gt 1 -a $cmd[1] = 'watson' ]
+  if [ (count $cmd) -eq 2 -a $cmd[1] = 'watson' ]
     if [ $argv[1] = $cmd[2] ]
       return 0
     end
@@ -21,26 +24,66 @@ function __fish_watson_get_projects
   command watson projects
 end
 
+function __fish_watson_get_tags
+  command watson tags
+end
+
+function __fish_watson_has_project
+  set cmd (commandline -opc)
+  if [ (count $cmd) -gt 2 -a $cmd[1] = 'watson' ]
+    if [ $argv[1] = $cmd[2] ]
+      if contains "$cmd[3]" (__fish_watson_get_projects)
+        return 0
+      end
+    end
+  end
+  return 1
+end
+
+function __fish_watson_get_frames -d "return a list of frames" #TODO, use watson logs to get more info
+  command watson frames
+end
+
+if [ -e ~/.config/watson/config ]
+  set url_string (command watson config backend.url 2> /dev/null)
+  if test -n "$url_string"
+    set url $url_string
+  end
+else
+  set url "a remote Crick server"
+end
+
+complete -f -c watson -n '__fish_watson_needs_sub' -a cancel -d "Cancel the last start command"
+complete -f -c watson -n '__fish_watson_needs_sub' -a config -d "Get and set configuration options"
+complete -f -c watson -n '__fish_watson_needs_sub' -a report -d "Display a report of time spent"
+complete -f -c watson -n '__fish_watson_needs_sub' -a restart -d "Restart monitoring time for a stopped project"
+complete -f -c watson -n '__fish_watson_needs_sub' -a status -d "Display when the current project was started and time spent"
+complete -f -c watson -n '__fish_watson_needs_sub' -a stop -d " Stop monitoring time for the current project"
+complete -f -c watson -n '__fish_watson_needs_sub' -a frames -d "Display the list of all frame IDs"
+complete -f -c watson -n '__fish_watson_needs_sub' -a help -d "Display help information"
+complete -f -c watson -n '__fish_watson_needs_sub' -a log -d "Display sessions during the given timespan"
+complete -f -c watson -n '__fish_watson_needs_sub' -a merge -d "merge existing frames with conflicting ones"
+complete -f -c watson -n '__fish_watson_needs_sub' -a projects -d "Display the list of projects"
+                      
+# sync                
+complete -f -c watson -n '__fish_watson_needs_sub' -a sync -d "sync your work with $url"
+                      
+complete -f -c watson -n '__fish_watson_needs_sub' -a tags -d "Display the list of tags"
+
+#edit
+complete -f -c watson -n '__fish_watson_needs_sub' -a edit -d "Edit a frame"
+complete -f -c watson -n '__fish_watson_using_command edit' -a "(__fish_watson_get_frames)"
 
 
-complete -f -n '__fish_watson_needs_sub' -c watson -a cancel
-complete -f -n '__fish_watson_needs_sub' -c watson -a config
-complete -f -n '__fish_watson_needs_sub' -c watson -a edit
-complete -f -n '__fish_watson_needs_sub' -c watson -a frames
-complete -f -n '__fish_watson_needs_sub' -c watson -a help
-complete -f -n '__fish_watson_needs_sub' -c watson -a log
-complete -f -n '__fish_watson_needs_sub' -c watson -a merge
-complete -f -n '__fish_watson_needs_sub' -c watson -a projects
-complete -f -n '__fish_watson_needs_sub' -c watson -a remove
-complete -f -n '__fish_watson_needs_sub' -c watson -a rename
-complete -f -n '__fish_watson_needs_sub' -c watson -a report
-complete -f -n '__fish_watson_needs_sub' -c watson -a restart
+#remove
+complete -f -c watson -n '__fish_watson_needs_sub' -a remove -d "Remove a frame"
+complete -f -c watson -n '__fish_watson_using_command remove' -a "(__fish_watson_get_frames)"
+
+#rename
+complete -f -c watson -n '__fish_watson_needs_sub' -a rename -d "Rename a project or tag"
+complete -f -c watson -n '__fish_watson_using_command rename' -a "(__fish_watson_get_projects) (__fish_watson_get_tags)"
 
 #start
-complete -f -c watson -n '__fish_watson_needs_sub' -a start
+complete -f -c watson -n '__fish_watson_needs_sub' -a start -d "Start monitoring time for a project"
 complete -f -c watson -n '__fish_watson_using_command start' -a "(__fish_watson_get_projects)"
-
-complete -f -n '__fish_watson_needs_sub' -c watson -a status
-complete -f -n '__fish_watson_needs_sub' -c watson -a stop
-complete -f -n '__fish_watson_needs_sub' -c watson -a sync
-complete -f -n '__fish_watson_needs_sub' -c watson -a tags
+complete -f -c watson -n '__fish_watson_has_project start' -a "+(__fish_watson_get_tags)"
